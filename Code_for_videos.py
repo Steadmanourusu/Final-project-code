77323973b699e70a925c3da7b49c7918033a77c7
Steadmanourusu <ourusu@gmail.com>
2021-05-17 09:56:37 +0100

    Update Code_for_videos.py

--- Code_for_videos.py
+++ Code_for_videos.py
@@ -59,15 +59,13 @@ coordinates = (-1,-1)
 floor= SIX
 
 def floor_number(floor, coordinates):
-        im = cv2.imread('C:\\Users\\ourus\\Documents\\4th_Year_Project\\OCR-full.jpg')
+        im = cv2.imread('C:\\Users\\ourus\\Documents\\4th_Year_Project\\OCR-full.jpg')   #Main template
         im = cv2.bitwise_not(im)
         imCopy = im.copy()
         imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
         ret,thresh = cv2.threshold(imgray,200,255,0)
         cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
             cv2.CHAIN_APPROX_SIMPLE)
-        #cnts = contours.sort_contours(cnts, method="left-to-right")[0]
-
         digits = {}
         locsx = []
         locsy = []
@@ -80,11 +78,6 @@ def floor_number(floor, coordinates):
         for c in cnts:
         # compute the bounding box of the contour
             (x, y, w , h) = cv2.boundingRect(c)
-        #    h = 75
-        #    w = 60
-        #    y = 22
-        #    cv2.rectangle(im, (x - 10, y - 10), (x + w + 10, y + h + 10), (0, 0, 255), 2)          #Resized the bouding box 
-        #    cv2.rectangle(thresh, (x - 10, y - 10), (x + w + 10, y + h + 10), (0, 0, 255), 1)     #by trial and erroring some values
             locs.append((x, y, w, h))
             locsx.append((x))
             locsy.append((y))
@@ -92,13 +85,7 @@ def floor_number(floor, coordinates):
             locsh.append((h))
             
 
-
-        #    print(x)
-        
         locsx = sorted(locsx, key=lambda b:b)
-        # locsy = sorted(locsy, key=lambda b:b)
-        # locsw = sorted(locsw, key=lambda b:b)
-        # locsh = sorted(locsh, key=lambda b:b)
         locs = sorted(locs, key=lambda b:b)
 
 
@@ -114,11 +101,8 @@ def floor_number(floor, coordinates):
         cv2.imwrite('C:\\Users\\ourus\\Documents\\4th_Year_project\\roi.jpg', roi)
 
         output = []
-        #img = cv2.imread('C:\\Users\\ourus\\Documents\\ssb1.jpg')
-        #img = cv2.imread('C:\\Users\\ourus\\Documents\\panel.jpg')
-        #img = cv2.imread('C:\\Users\\ourus\\Documents\\ssb.jpg')
 
-        fps= (30*5)#*3*0
+        fps= (30*5)
 
 
         while(True):
@@ -127,8 +111,6 @@ def floor_number(floor, coordinates):
             cap.set(1,fps)           #cap.set(1,fps+(frame_per_second*4))
             total_number_of_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
             frame_per_second = cap.get(cv2.CAP_PROP_FPS)
-            # print("fps=",fps)
-            # print("total_number_of_frames=",total_number_of_frames)
             return_value,image = cap.read()#
 
             #standardise fram size
@@ -139,7 +121,6 @@ def floor_number(floor, coordinates):
             # resize image
             image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
 
-        #    rotated = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
             cv2.imwrite( "C:\\Users\\ourus\\Documents\\4th_Year_Project\\img.png", image)
             time.sleep(10)
 
@@ -153,8 +134,7 @@ def floor_number(floor, coordinates):
             opening = cv2.morphologyEx(imgcpy, cv2.MORPH_OPEN, kernel)      #cleans up the image
 
             template = cv2.imread('C:\\Users\\ourus\\Documents\\4th_Year_project\\roi.jpg',0)
-            #roi = cv2.bitwise_not(roi)
-            # Resizes the letters so that they can match properly
+
             scale_percent = 140
             if floor == D:
                 scale_percent = 50
@@ -164,15 +144,13 @@ def floor_number(floor, coordinates):
                 scale_percent = scale_percent + 5
             elif scale_percent!= E:
                 pass
-        #    print(scale_percent)
             x=0
             n=1
-
             while scale_percent < 300:
                 if fps >= total_number_of_frames:
                     break
                 threshold = 0.8
-                while threshold>=0.65 and x==0:     #while threshold>=0.65 and x==0:
+                while threshold>=0.65 and x==0:    
                     print("threshold =", threshold)
                     width = int(template.shape[1]*scale_percent/100)
                     height = int(template.shape[0]*scale_percent/100)
@@ -187,7 +165,6 @@ def floor_number(floor, coordinates):
                     loc = np.where(res >= threshold)
                     loc1 = list(zip(*loc[::-1]))
                     x=len(loc1)
-        #            print(x)
 
                     for pt in zip(*loc[::-1]):
                         cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
@@ -200,16 +177,6 @@ def floor_number(floor, coordinates):
                 scale_percent += 5
             cv2.imwrite('C:\\Users\\ourus\\Documents\\4th_Year_project\\img1.jpg', img)
             img1 = cv2.imread('C:\\Users\\ourus\\Documents\\4th_Year_Project\\img1.jpg')
-            # scale_percent = 30 # percent of original size
-            # width = int(img1.shape[1] * scale_percent / 100)
-            # height = int(img1.shape[0] * scale_percent / 100)
-            # dim = (width, height)
-        
-            # # resize image
-            #img1 = cv2.resize(img1, dim, interpolation = cv2.INTER_AREA)
-            #cv2.imshow("img1", img1)
-            #cv2.waitKey(5000)
-            
             cap.release()
             cv2.destroyAllWindows()
 
@@ -228,21 +195,12 @@ def floor_number(floor, coordinates):
             
         print("fps=", fps)
 
-
-    
-        # print(res)                                                                                    #TEST POINT
         scale_percent = 90 # percent of original size                                                     
         width = int(img.shape[1] * scale_percent / 100)
         height = int(img.shape[0] * scale_percent / 100)
         dim = (width, height)
-        
-        # resize image
-        #img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 
-
-       
-        #print('Resized Dimensions : ',resized.shape)
-        #HERE
+        #TEST POINT
         cv2.imshow("img", img)
         #cv2.imshow("img1", opening)
         #cv2.imshow("roi", roi)
@@ -252,10 +210,9 @@ def floor_number(floor, coordinates):
 
 
         cv2.waitKey(0)
-        #print(scale_percent)
-        #########################################################################################################################
 
-        # NOT TO SELF - keep the time of the video [number of frames] in mind when prepping the video for for the presentation#
+        #########################################################################################################################
+        
         print("image1 size = ", img1.shape)
         print("image size = ", img.shape)
         return coordinates
@@ -273,16 +230,15 @@ def symbols(floor, coordinates):
         while(True):
             cap = cv2.VideoCapture('C:\\Users\\ourus\\Documents\\4th_Year_project\\Demo\\I.P.demo.mp4')
             frame_per_second = cap.get(cv2.CAP_PROP_FPS)
-            cap.set(1,fps)           #cap.set(1,fps+(frame_per_second*4))
+            cap.set(1,fps)           
             total_number_of_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
             frame_per_second = cap.get(cv2.CAP_PROP_FPS)
-            # print("fps=",fps)
-            # print("total_number_of_frames=",total_number_of_frames)
+
             return_value,image = cap.read()
 
-            #standardise fram size
-            width = 1080                   #1950                          #1080
-            height =  1920                 #1110                         #1920
+            #standardise frame size
+            width = 1080                   
+            height =  1920                
             dim = (width, height)
  
             # resize image
@@ -290,7 +246,6 @@ def symbols(floor, coordinates):
 
 
             cv2.imwrite( "C:\\Users\\ourus\\Documents\\4th_Year_Project\\img.png", image)
-            #time.sleep(10)
 
             img = cv2.imread('C:\\Users\\ourus\\Documents\\4th_Year_Project\\img.png')
             img = cv2.bitwise_not(img )
@@ -299,12 +254,10 @@ def symbols(floor, coordinates):
             imgcpy = cv2.adaptiveThreshold(imgcpy, 225, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11,2) #adaptive thresholding after greyscaling
             imgcpy = cv2.bitwise_not(imgcpy)  
             kernel = np.array([[0., 1., 0.], [1., 1., 1.], [0., 1., 0.]],np.uint8)
-            opening = cv2.morphologyEx(imgcpy, cv2.MORPH_OPEN, kernel)      #cleans up the image
+            opening = cv2.morphologyEx(imgcpy, cv2.MORPH_OPEN, kernel)      #Cleans up the image
 
             template = imgray
-            #roi = cv2.bitwise_not(roi)
-            # Resizes the letters so that they can match properly
-            scale_percent = 15                  #20
+            scale_percent = 15                  
             if floor == alarm:
                 scale_percent = 70
             elif scale_percent!= alarm:
@@ -344,12 +297,6 @@ def symbols(floor, coordinates):
                     for pt in zip(*loc[::-1]):
                         cv2.rectangle(opening, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
                         coordinates = ( ( ( (pt[0] + w, pt[1] + h)[0] - pt[0])/2 ) + pt[0] , ( ( (pt[0] + w, pt[1] + h)[1] - pt[1])/2 ) + pt[1] )
-                        # print("TL =", pt)
-                        # print("BR =", (pt[0] + w, pt[1] + h))
-                        # jx = ( ( (pt[0] + w, pt[1] + h)[0] - pt[0])/2 ) + pt[0]
-                        # jy = ( ( (pt[0] + w, pt[1] + h)[1] - pt[1])/2 ) + pt[1]
-                        # print("j.x =", jx)
-                        # print("j.y = ", jy)
                     threshold-=0.01
                 scale_percent += 5
             
@@ -362,9 +309,6 @@ def symbols(floor, coordinates):
         
             # resize image
             img1 = cv2.resize(img1, dim, interpolation = cv2.INTER_AREA)
-            #cv2.imshow("img1", img1)
-            #cv2.waitKey(5000)
-            
             cap.release()
             cv2.destroyAllWindows()
 
@@ -383,29 +327,12 @@ def symbols(floor, coordinates):
             
         print("fps=", fps)
   
-
-
-        # print(res)
-
-                                        # scale_percent = 30 # percent of original size
-                                        # width = int(img.shape[1] * scale_percent / 100)
-                                        # height = int(img.shape[0] * scale_percent / 100)
-                                        # dim = (width, height)
-                                        
-                                        # # resize image
-                                        # img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
         scale_percent = 30 # percent of original size
         width = int(img.shape[1] * scale_percent / 100)
         height = int(img.shape[0] * scale_percent / 100)
         dim = (width, height)
-        
-        # resize image
-        #        img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-        
-        #        img = cv2.circle(img, (186,1489), radius=20, color=(255, 255,255), thickness=-500)
-        
-        #print('Resized Dimensions : ',resized.shape)
-        #HERE
+ 
+        #TEST POINT
         cv2.imshow("img", img)
         #cv2.imshow("img1", opening)
         #cv2.imshow("roi", template)
@@ -415,10 +342,8 @@ def symbols(floor, coordinates):
 
 
         cv2.waitKey(0)
-        #print(scale_percent)
-        #########################################################################################################################
 
-        # NOT TO SELF - keep the time of the video [number of frames] in mind when prepping the video for for the presentation#
+        #########################################################################################################################
         print("image size = ", img1.shape)
         print("image size = ", img.shape)
 
@@ -433,7 +358,6 @@ def sort_contours(cnts, method="left-to-right"):
     return (cnts, boundingBoxes)
 
 
-
 ################################## MAIN ######################################
 if floor == alarm or floor == close_door or floor == open_door :    
     coordinates = symbols(floor, coordinates)
@@ -444,4 +368,3 @@ if coordinates == (-1,-1):
     print("No button found")
 else:
     print("coordinates = ", coordinates)
-
